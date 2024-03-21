4bd2df89776d1e3edb1b9b6a974a644fd6345a3a
philippe.bouchet <philippe.bouchet@epita.fr>
2024-03-21 08:42:18 +0100

    Added grayscale implementation

--- src/gpu/grayscale.cu
+++ src/gpu/grayscale.cu
@@ -1,6 +1,18 @@
 #include <GPU_ops.cuh>
 
-__global__ void GPU::grayscale()
+__global__ void GPU::grayscale(unsigned char* d_dst, const unsigned char* d_src, int width, int height)
 {
+    int dim = blockDim.x * blockIdx.x + threadIdx.x;
 
+    int x = dim % width;
+    int y = dim / width;
+
+    if (x >= width || y >= height)
+        return;
+
+    int r = d_src[dim * 3];
+    int g = d_src[dim * 3 + 1];
+    int b = d_src[dim * 3 + 2];
+
+    d_dst[dim] = static_cast<unsigned char>((r + g + b) / 3);
 }
