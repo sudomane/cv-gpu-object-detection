6399d36458ba7324b55256554906e79600caf0a9
philippe.bouchet <philippe.bouchet@epita.fr>
2024-03-21 11:12:57 +0100

    Added GPU binary

--- include/GPU_ops.cuh
+++ include/GPU_ops.cuh
@@ -10,6 +10,6 @@ namespace GPU
     __global__ void grayscale  (unsigned char* d_dst, const unsigned char* d_src, int width, int height);
     __global__ void difference (unsigned char* d_dst, const unsigned char* d_src, int width, int height);
     __global__ void gaussian   (unsigned char* d_dst, const unsigned char* d_src, const float* d_kernel, int width, int height, int kernel_size, int sigma, int offset);
-    __global__ void morphology (unsigned char* d_dst, const unsigned char* d_src, int width, int height, int opening_size, int closing_size)
-
+    __global__ void morphology (unsigned char* d_dst, const unsigned char* d_src, int width, int height, int opening_size, int closing_size);
+    __global__ void binary     (unsigned char* d_dst, int bin_thresh, int width, int height);
 }; // namespace cpu
