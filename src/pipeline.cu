7a9ad8f631406609cb56d6ec0a2895b26b93914d
philippe.bouchet <philippe.bouchet@epita.fr>
2024-03-21 11:01:37 +0100

    Updated pipeline

--- src/pipeline.cu
+++ src/pipeline.cu
@@ -1,14 +1,16 @@
 #include <pipeline.hpp>
 
 #include <err.h>
+#include <cmath>
 #include <GPU_ops.cuh>
 #include <opencv2/opencv.hpp>
 
-static inline unsigned char* _cudaMalloc(size_t n)
+template<typename T>
+static inline T* _cudaMalloc(size_t n)
 {
-    unsigned char* d_out;
+    T* d_out;
 
-    int rc = cudaMalloc(&d_out, n);
+    int rc = cudaMalloc(&d_out, sizeof(T) * n);
     if (rc)
       errx(1, "Failed to allocate d_out.");
 
@@ -17,7 +19,7 @@ static inline unsigned char* _cudaMalloc(size_t n)
 
 static inline unsigned char* _toDevice(const unsigned char* h_src, int width, int height, int n_channels = 1)
 {
-    unsigned char* d_dst = _cudaMalloc(sizeof(unsigned char) * width * height * n_channels);
+    unsigned char* d_dst = _cudaMalloc<unsigned char>(width * height * n_channels);
 
     int rc = cudaMemcpy(d_dst, h_src, sizeof(unsigned char) * width * height * n_channels, cudaMemcpyHostToDevice);
     if (rc)
@@ -56,7 +58,7 @@ static inline unsigned char* _initRef(unsigned char* h_ref_image, const t_point&
 
     unsigned char* h_ref_gray  = new unsigned char[width * height];
 
-    unsigned char* d_ref_gray  = _cudaMalloc(width * height * sizeof(unsigned char));
+    unsigned char* d_ref_gray  = _cudaMalloc<unsigned char>(width * height);
     unsigned char* d_ref_image = _toDevice(h_ref_image, width, height, 3);
 
     GPU::grayscale<<<num_blocks, block_size>>>(d_ref_gray, d_ref_image, width, height);
@@ -66,6 +68,40 @@ static inline unsigned char* _initRef(unsigned char* h_ref_image, const t_point&
     return d_ref_gray;
 }
 
+static inline float* _generateDeviceKernel(int kernel_size, float sigma)
+{
+    float  sum = 0;
+    float* h_kernel = new float[kernel_size * kernel_size];
+    float* d_kernel = _cudaMalloc<float>(kernel_size * kernel_size);
+
+    for (int i = 0; i < kernel_size; i++)
+    {
+        for (int j = 0; j < kernel_size; j++)
+        {
+            float x = i - kernel_size / 2;
+            float y = j - kernel_size / 2;
+
+            float val = std::exp2f(-(x*x + y*y) / (2 * sigma * sigma)) / (2 * M_PI * sigma * sigma);
+            h_kernel[j * kernel_size + i] = val;
+
+            sum += val;
+        }
+    }
+
+    for (int i = 0; i < kernel_size; i++)
+    {
+        for (int j = 0; j < kernel_size; j++)
+        {
+            h_kernel[j * kernel_size + i] /= sum;
+        }
+    }
+
+    cudaMemcpy(d_kernel, h_kernel, kernel_size * kernel_size * sizeof(float), cudaMemcpyHostToDevice);
+
+    delete[] h_kernel;
+
+    return d_kernel;
+}
 
 void GPU::runPipeline(std::vector<std::pair<std::string, unsigned char*>>& images,
                      const std::pair<int, int> &dim)
@@ -73,31 +109,39 @@ void GPU::runPipeline(std::vector<std::pair<std::string, unsigned char*>>& image
     int width  = std::get<0>(dim);
     int height = std::get<1>(dim);
 
+    int sigma         = 10;
+    int kernel_size   = 21;
+    int kernel_offset = std::floor(kernel_size / 2);
+    float* d_kernel   = _generateDeviceKernel(kernel_size, sigma);
+
     int block_size = 256;
     int num_blocks = (width * height + block_size - 1) / block_size;
 
-    unsigned char* d_ref = _initRef(std::get<1>(images[0]), dim);
+    unsigned char* d_ref     = _initRef(std::get<1>(images[0]), dim);
+    unsigned char* d_buffer  = _cudaMalloc<unsigned char>(width * height * sizeof(unsigned char));
+    unsigned char* d_buffer_ = _cudaMalloc<unsigned char>(width * height * sizeof(unsigned char));
 
     for (int i = 1; i < images.size(); i++)
     {
-        if (i != 20)
-            continue;
-
         const std::string filename = std::get<0>(images[i]);
         unsigned char* h_image     = std::get<1>(images[i]);
         unsigned char* d_image     = _toDevice(h_image, width, height, 3);
-        unsigned char* d_buffer    = _cudaMalloc(width * height * sizeof(unsigned char));
 
         GPU::grayscale <<<num_blocks, block_size>>>(d_buffer, d_image, width, height);
         GPU::difference<<<num_blocks, block_size>>>(d_buffer, d_ref, width, height);
+        GPU::gaussian  <<<num_blocks, block_size>>>(d_buffer_, d_buffer, d_kernel,
+                                                    width, height, kernel_size,
+                                                    sigma, kernel_offset);
 
-        _saveImage(d_buffer, dim, "out" + std::to_string(i) + ".png");
+        _saveImage(d_buffer_, dim, "out" + std::to_string(i) + ".png");
 
         std::cout << "[GPU] : " << i << "/" << images.size()-1 << std::endl;
 
-        cudaFree(d_buffer);
         cudaFree(d_image);
     }
 
+    cudaFree(d_buffer);
+    cudaFree(d_buffer_);
     cudaFree(d_ref);
+    cudaFree(d_kernel);
 }
