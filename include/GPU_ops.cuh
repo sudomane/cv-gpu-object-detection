65496342a592a30d4e7b17779b2d7cb2565258aa
philippe.bouchet <philippe.bouchet@epita.fr>
2024-03-21 11:01:12 +0100

    Added gaussian conv

--- include/GPU_ops.cuh
+++ include/GPU_ops.cuh
@@ -9,6 +9,7 @@ namespace GPU
 {
     __global__ void grayscale  (unsigned char* d_dst, const unsigned char* d_src, int width, int height);
     __global__ void difference (unsigned char* d_dst, const unsigned char* d_src, int width, int height);
-    __global__ void gaussian   (unsigned char* d_dst, int width, int height, int kernel_size, int sigma);
+    __global__ void gaussian   (unsigned char* d_dst, const unsigned char* d_src, const float* d_kernel, int width, int height, int kernel_size, int sigma, int offset);
+    __global__ void morphology (unsigned char* d_dst, const unsigned char* d_src, int width, int height, int opening_size, int closing_size)
 
 }; // namespace cpu
