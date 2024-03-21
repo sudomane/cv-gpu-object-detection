2c348fbdb8301473dfbbcdba85e4d17ee1806720
philippe.bouchet <philippe.bouchet@epita.fr>
2024-03-21 08:41:24 +0100

    Added header definitions for diff., gray. and Gaussian in CUDA.

--- include/GPU_ops.cuh
+++ include/GPU_ops.cuh
@@ -7,5 +7,8 @@ typedef std::pair<int,int> t_point;
 
 namespace GPU
 {
-    __global__ void grayscale ();
+    __global__ void grayscale  (unsigned char* d_dst, const unsigned char* d_src, int width, int height);
+    __global__ void difference (unsigned char* d_dst, const unsigned char* d_src, int width, int height);
+    __global__ void gaussian   (unsigned char* d_dst, int width, int height, int kernel_size, int sigma);
+
 }; // namespace cpu
