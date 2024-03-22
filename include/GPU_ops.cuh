6d0d2e2bc34219ccf1410c2929d43cc6d717594a
philippe.bouchet <philippe.bouchet@epita.fr>
2024-03-22 09:51:18 +0100

    Added 3rd buffer for morphology

--- include/GPU_ops.cuh
+++ include/GPU_ops.cuh
@@ -84,8 +84,8 @@ namespace GPU
     __global__ void grayscale  (unsigned char* d_dst, const unsigned char* d_src, int width, int height);
     __global__ void difference (unsigned char* d_dst, const unsigned char* d_src, int width, int height);
     __global__ void gaussian   (unsigned char* d_dst, const unsigned char* d_src, const float* d_kernel, int width, int height, int kernel_size, int offset);
-    __global__ void morphology (unsigned char* d_dst, unsigned char* d_src, int width, int height, int opening_size, int closing_size, int offset);
+    __global__ void morphology (unsigned char* d_dst, unsigned char* d_src, unsigned char* d_buf, int width, int height, int opening_size, int closing_size, int opening_offset, int closing_offset);
     __global__ void binary     (unsigned char* d_dst, int bin_thresh, int width, int height);
     __global__ void components (unsigned char* d_dst, int width, int height);
-    __global__ void getBbox(unsigned char* d_src, int* coords, int width, int height, int label);
+    __global__ void getBbox    (unsigned char* d_src, int* coords, int width, int height, int label);
 }; // namespace gpu
