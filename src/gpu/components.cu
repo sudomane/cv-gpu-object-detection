7898af44635db6d1a2f35fabafbce2290b59a7f2
philippe.bouchet <philippe.bouchet@epita.fr>
2024-03-25 13:17:29 +0100

    Added init label algorithm for CC

--- src/gpu/components.cu
+++ src/gpu/components.cu
@@ -1,11 +1,21 @@
 #include <GPU_ops.cuh>
 
-__global__ static void _initLabel(unsigned char* d_label)
+__global__ void GPU::initLabelCC(int* d_label, int width, int height)
 {
+    int dim = blockDim.x * blockIdx.x + threadIdx.x;
 
+    int x = dim % width;
+    int y = dim / width;
+
+    if (x>= width || y >= height)
+        return;
+
+    int pos = x * height + y;
+
+    d_label[pos] = pos;
 }
 
-__global__ void GPU::components(unsigned char* d_data, unsigned char* d_labels, int width, int height)
+__global__ void GPU::components(unsigned char* d_data, int* d_labels, int width, int height)
 {
 
 }
